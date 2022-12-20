565d65bfd56d44273df7e035a8e5d49272a4821d
Pnda0 <alan_carvalho2015@hotmail.com>
2022-12-20 10:18:12 -0300

    calculadora conquer

---
+++ Calculadora/main.js
@@ -0,0 +1,56 @@
+function calculadora() {
+
+    const operacao = Number(prompt('Escolha uma operação:\n 1 - Soma (+)\n 2 - Subtração (-)\n 3 - Multiplicação\n 4 - Divisão\n 5 - Divisão inteira(%)\n 6 - Potenciação (**)'))
+
+// O sinal de ! significa: "Diferente de"->
+// O sinais || significa: "Ou" ->
+    if(!operacao || operacao >= 7){
+        alert("Erro - Operação inválida!");
+        calculadora();
+    }
+    else {
+        let n1 = Number(prompt("Insira o primeiro valor:"));
+        let n2 = Number(prompt("Insira o segundo valor:"));
+        let resultado;
+        
+        if(!n1 || !n2) {
+            alert("Erro - Opção inválida");
+            calculadora();
+        }
+        else {
+            function Soma() {
+                resultado = n1 + n2;
+                // O sinais $ significa: "Formatar" ->
+                alert(' ${n1} + ${n2} = ${resultado}');
+            }
+            function Subtração() {
+                resultado = n1 - n2;
+                // O sinais $ significa: "Formatar" ->
+                alert(' ${n1} - ${n2} = ${resultado}');
+            }
+            function Multiplicação() {
+                resultado = n1 * n2;
+                // O sinais $ significa: "Formatar" ->
+                alert(' ${n1} * ${n2} = ${resultado}');
+            }
+            function Divisão() {
+                resultado = n1 / n2;
+                // O sinais $ significa: "Formatar" ->
+                alert(' ${n1} / ${n2} = ${resultado}');
+            }
+            function Divisão_inteira() {
+                resultado = n1 % n2;
+                // O sinais $ significa: "Formatar" ->
+                alert(' ${n1} % ${n2} = ${resultado}');
+            }
+            function Potenciação() {
+                resultado = n1 ** n2;
+                // O sinais $ significa: "Formatar" ->
+                alert(' ${n1} ** ${n2} = ${resultado}');
+            }
+        }
+        }
+    }
+
+calculadora()
+
