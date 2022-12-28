616885fa5c44b0af07e1a771520f92dc63e37a76
Pnda0 <alan_carvalho2015@hotmail.com>
2022-12-27 22:46:40 -0300

    Novos Exercicios

---
+++ Exercicios/Conquer Plus/Break/Break.js
@@ -0,0 +1,9 @@
+const lista = [0,1, 1, 2, 3, 5, 8, 18, 21, 34, 55,89, 144,];
+
+for (let number of lista) {
+    if (number > 50){
+    break;
+    }
+}
+console.log(number);
+
---
+++ Exercicios/Conquer Plus/Break/Exercicio2.js
@@ -0,0 +1,17 @@
+
+let i = 0;
+let total = 20;
+
+// Percorre de 0 a 20
+
+while (i < 20) {
+    i++;
+
+    if (i > 5) continue;
+
+    if (i > 6 < 15){
+        console.log(i)
+        break;
+    }
+}
+
