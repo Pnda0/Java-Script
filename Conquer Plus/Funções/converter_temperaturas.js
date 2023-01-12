78ec42f661dfcd62938fb70d9e9021ee8d6ed95b
devadpromotora <desenvolvimento@adpromotora.com.br>
2023-01-12 10:05:48 -0300

    Java

--- Conquer Plus/Funções/converter_temperaturas.js
+++ Conquer Plus/Funções/converter_temperaturas.js
@@ -2,6 +2,6 @@ function temperatura(celsius){
     return celsius * 1.8 + 32
 }
 
-console.log("Temperatura hoje de lagarto em Farenheint é de:" +temperatura(34))
+console.log("Temperatura hoje de lagarto em Farenheint é de:" + temperatura(34))
 
 // 93.2
---
+++ Conquer Plus/Funções/função_adição.js
@@ -0,0 +1,7 @@
+
+function adicao(num1, num2){
+   return num1, num2 =  34 + 40
+}
+
+
+console.log( "Adição total brother é " + adicao(30, 20))
