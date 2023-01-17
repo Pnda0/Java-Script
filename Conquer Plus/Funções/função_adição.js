d2f7afa5b33c7b964e5a667dd14a82cd4eedc6a3
devadpromotora <desenvolvimento@adpromotora.com.br>
2023-01-17 16:10:56 -0300

    Javinha

--- Conquer Plus/Funções/função_adição.js
+++ Conquer Plus/Funções/função_adição.js
@@ -3,5 +3,14 @@ function adicao(num1, num2){
    return num1, num2 =  34 + 40
 }
 
-
 console.log( "Adição total brother é " + adicao(30, 20))
+
+// Tipo 2 
+
+function add (x, y){
+   return x + y;
+}
+
+let soma = add(30, 200);
+
+console.log(soma)
