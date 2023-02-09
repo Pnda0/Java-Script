0fe9b392aedf9c0fe836197b680e6f7dd1bbb567
devadpromotora <alan_carvalho2015@hotmail.com>
2023-02-09 18:00:31 -0300

    adicionando

---
+++ Conquer Plus/Java_Html/adicionando_texto.js
@@ -0,0 +1,15 @@
+const codigo_postal = document.getElementById("zipCode")
+
+console.log(codigo_postal.textContent = "7979779")
+
+const endereco = document.getElementById("Adress")
+
+console.log(endereco.textContent = "Pururuca")
+
+const numero = document.getElementById('Number')
+
+console.log(numero.textContent = "96")
+
+const complemento = document.getElementById('Complemento')
+
+console.log(complemento.textContent = "Frei")
