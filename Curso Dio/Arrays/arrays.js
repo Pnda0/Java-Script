c619c3d6d04c20ff052853cdcdfc6e34124f1064
Pnda0 <alan_carvalho2015@hotmail.com>
2024-03-27 21:29:01 -0300

    Aulinha de leve

---
+++ Curso Dio/Arrays/arrays.js
@@ -0,0 +1,15 @@
+// lista
+const alunos = ['Yan', 'Savio']
+
+// Adicionando itens na lista
+alunos.push('Alex')
+
+// Adicionando na posição exata ou substituir
+alunos[3] = 'Binho'
+console.log(alunos)
+
+// Removendo o último e o primeiro item
+alunos.pop()
+ 
+alunos.shift()
+console.log(alunos)
---
+++ Curso Dio/Arrays/estruturasSemRepeticao.js
@@ -0,0 +1,18 @@
+// Adicionando itens a uma lista vazia
+const notas = []
+
+notas.push(5);
+notas.push(9);
+notas.push(10);
+notas.push(5);
+notas.push(2);
+
+// Verificando a quantidade de itens numa lista
+console.log(notas.length)
+
+// Pegar a média da lista; soma / tamanho da lista
+const soma = notas[0] +  notas[1] + notas[2] + notas[3] + notas[4]
+console.log(soma / notas.length)
+
+
+
