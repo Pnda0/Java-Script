28d76d25908ede38d79b20b4f3c3a65bfbc6baeb
Pnda0 <alan_carvalho2015@hotmail.com>
2024-04-16 15:12:38 -0300

    teste

---
+++ Testes/novoteste.js
@@ -0,0 +1,24 @@
+function run() {
+    var agora = new Date();
+    var diaSemana = agora.getDay();
+    var horaAtual = agora.getHours().toString().padStart(2, '0') + ":" + agora.getMinutes().toString().padStart(2, '0');
+  
+    var horaLimiteFim = "17:30";
+  
+    if (diaSemana === 6) {
+      horaLimiteFim = "17:00";
+    }
+  
+    var horaLimiteInicio = "08:00";
+    var status = horaAtual >= horaLimiteInicio && horaAtual <= horaLimiteFim && (diaSemana >= 1 && diaSemana <= 5) ? "Aberto" : "Fechado";
+  
+    return {
+      status: status,
+      horaAtual: horaAtual
+    };
+  }
+
+  var resultado = run();
+
+  console.log("Status:", resultado.status);
+  console.log("Hora atual:", resultado.horaAtual);
