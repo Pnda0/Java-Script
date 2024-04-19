5f48344390f34ee317d411720e988ec422a26432
Pnda0 <alan_carvalho2015@hotmail.com>
2024-04-19 12:09:06 -0300

    subida de leve

---
+++ Trybe/Dom/criandoElementos.js
@@ -0,0 +1,29 @@
+// Criando e adicionando elementos
+
+const achandoFilho = document.getElementById('primeiroFilhoDoFilho');
+
+const criandoIrmao = document.createElement('li');
+criandoIrmao.innerText = 'Fala bro, to aqui';
+
+achandoFilho.appendChild(criandoIrmao)
+
+// Adicionando uma lista já pronta
+const criandoLista = ['Barcelona', 'PSG', 'REAL MADRID']
+
+const terceiroFilho = document.getElementById('terceiroFilho');
+
+for (let i = 0; i < criandoLista.length; i++) {
+    const time = criandoLista[i];
+    
+    let criandoListaitens = document.createElement('li')
+    criandoListaitens.innerText = time
+
+    terceiroFilho.appendChild(criandoListaitens)
+}
+
+// Removendo elementos
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
